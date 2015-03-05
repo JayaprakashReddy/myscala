84dab936a330105d5164698e1252e8744fa9ceba
jayaprakashreddy <jayaprakashreddy@gmail.com>
2015-03-05 16:58:48 +1100

    some updates

---
+++ src/main/scala/com/jpreddy/scala/ScalaCookBookNumbersDriver.scala
@@ -0,0 +1,16 @@
+package com.jpreddy.scala
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+object ScalaCookBookNumbersDriver extends App{
+
+val logger = LoggerFactory.getLogger("com.jpreddy.scala");
+
+logger.debug("ScalaCookBookNumbersDriver starting")
+//
+println("Test")
+
+logger.debug("ScalaCookBookNumbersDriver exiting")
+
+}
--- src/main/scala/com/jpreddy/scala/ScalaCookBookStringsDriver.scala
+++ src/main/scala/com/jpreddy/scala/ScalaCookBookStringsDriver.scala
@@ -8,11 +8,14 @@ object ScalaCookBookStringsDriver extends App{
 val logger = LoggerFactory.getLogger("com.jpreddy.scala");
 
 logger.debug("ScalaCookBookDriver starting")
-
+//In Scala, the == method defined in the AnyRef class first checks for null values, 
+//and then calls the equals method on the first object (i.e., this) to see if the two objects are equal. 
 val s1="Hello"
 val s2= new String("Hello")
 println(s1==s2)
 println(s1 == null)
+
+
 
 logger.debug("ScalaCookBookDriver starting")
 
